37562fa547d5d4a29840d4336a3d43052ecfcbeb
arvinxx <arvinx@foxmail.com>
2023-06-10 16:07:13 +0800

    :sparkles: feat: 升级 changelog 配置

--- .changelogrc.js
+++ .changelogrc.js
@@ -2,4 +2,8 @@
 module.exports = {
   displayTypes: ['feat', 'fix', 'styles', 'pref', 'build'],
   titleLanguage: 'zh-CN',
+  showSummary: true,
+  reduceHeadingLevel: true,
+  newlineTimestamp: true,
+  addBackToTop: true,
 };

41d342d7e3efa66219afa8264ae4c55e19e6e378
arvinxx <arvinx@foxmail.com>
2023-06-10 16:32:47 +0800

    :bug: fix: update release workflow

--- .changelogrc.js
+++ .changelogrc.js
@@ -2,6 +2,8 @@
 module.exports = {
   displayTypes: ['feat', 'fix', 'styles', 'pref', 'build'],
   titleLanguage: 'zh-CN',
+  showAuthor: true,
+  showAuthorAvatar: true,
   showSummary: true,
   reduceHeadingLevel: true,
   newlineTimestamp: true,
